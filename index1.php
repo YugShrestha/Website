f87a3af14b5a75039e34711c509988caefb5b3f6
ystha <yug.shrestha1@gmail.com>
2023-07-27 21:29:54 +0545

    changes

--- index1.php
+++ index1.php
@@ -8,12 +8,15 @@ $routes=[
     '/website/index1.php/about.php'=>'controller/index.php',
     '/website/index1.php/contact.php'=>'controller/index.php'
 ];
-  if(array_key_exists($uri,$routes)){
-    require $routes[$uri];
-  }else {
-  http_response_code(404);
+function abort(){
+    http_response_code(404);
  
   require "Views/404.php";
   die();
+}
+  if(array_key_exists($uri,$routes)){
+    require $routes[$uri];
+  }else{
 
+  abort();
   }
