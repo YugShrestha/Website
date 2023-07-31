77d48e257747c34dd9d72fb7abd495657f08d008
ystha <yug.shrestha1@gmail.com>
2023-08-01 00:00:46 +0545

    changes

--- index1.php
+++ index1.php
@@ -1,13 +1,20 @@
 <?php
 
 require("function.php"); 
-require "router.php";
-require("database.php");
+//require "router.php";
+
+
+$dsn="mysql:host=localhost;port=3306;dbname=demo;";
+
+$pdo= new PDO($dsn,'root');// pdo class is already creeated 
+
+$statemnet =$pdo->prepare("select * from  post");
+
+$statemnet->execute();
 
 
 
 
+$post= $statemnet->fetchAll();
 
-
-
-
+dd($post);
